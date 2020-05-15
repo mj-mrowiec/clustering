4dd89eb224edfd846dbb391fd92a963d60c12954
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-15 21:30:11 +0200

    depth 3 for decision_trees

--- decision_trees.py
+++ decision_trees.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 
+
 # Create a random dataset
 rng = np.random.RandomState(1)
 X = np.sort(5 * rng.rand(80, 1), axis=0)
@@ -11,14 +12,17 @@ y[::5] += 3 * (0.5 - rng.rand(16))
 
 # Fit regression model
 regr_1 = DecisionTreeRegressor(max_depth=2)
-regr_2 = DecisionTreeRegressor(max_depth=5)
+regr_2 = DecisionTreeRegressor(max_depth=3)
+regr_3 = DecisionTreeRegressor(max_depth=5)
 regr_1.fit(X, y)
 regr_2.fit(X, y)
+regr_3.fit(X, y)
 
 # Predict
 X_test = np.arange(0.0, 5.0, 0.01)[:, np.newaxis]
 y_1 = regr_1.predict(X_test)
 y_2 = regr_2.predict(X_test)
+y_3 = regr_3.predict(X_test)
 
 # Plot the results
 plt.figure()
@@ -26,7 +30,8 @@ plt.scatter(X, y, s=20, edgecolor="black",
             c="darkorange", label="data")
 plt.plot(X_test, y_1, color="cornflowerblue",
          label="max_depth=2", linewidth=2)
-plt.plot(X_test, y_2, color="yellowgreen", label="max_depth=5", linewidth=2)
+plt.plot(X_test, y_2, color="yellowgreen", label="max_depth=3", linewidth=2)
+plt.plot(X_test, y_3, color="darkred", label="max_depth=5", linewidth=2)
 plt.xlabel("data")
 plt.ylabel("target")
 plt.title("Decision Tree Regression")
