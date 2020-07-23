02d708bc67491b4ef8d3cb8e67cfdcc21dee5adc
mj-mrowiec <mjmrowiec@gmail.com>
2020-07-23 22:04:58 +0200

    adding plotting

--- kernel.py
+++ kernel.py
@@ -9,3 +9,8 @@ h = plt.hist(x, bins=30, normed=True, color=(0,.5,0,1), label='Histogram')
 plt.plot(xs, ys, 'r--', linewidth=2, label='$\mathcal{N}(0,1)$')
 plt.xlim(-3,3)
 plt.xlabel('X')
+
+from pyqt_fit import kde
+est = kde.KDE1D(x)
+plot(xs, est(xs), label='Estimate (bw={:.3g})'.format(est.bandwidth))
+plt.legend(loc='best')
